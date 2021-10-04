e502594a4d23685fcf995f5960400d199d1a133d
an.ngo <phowflowf135@gmail.com>
2021-10-04 11:28:47 +0700

    m

--- binarySearch/src/BinarySearch.java
+++ binarySearch/src/BinarySearch.java
@@ -1,14 +1,31 @@
 import java.util.Arrays;
-import java.util.Collections;
 
 public class BinarySearch {
 
     public static void main(String[] args) {
     int [] arr = {9,8,6,7,4,5,3,1,2,0};
-        Arrays.sort(arr);
         for (int i :
                 arr) {
-            System.out.println(i);
+            Arrays.sort(arr);
         }
+        int findNum = 5;
+        System.out.println(findNumMethod(arr,findNum));
+    }
+
+    private static int findNumMethod(int[] arr, int findNum) {
+        int min = 0;
+        int max = arr.length;
+        int mid ;
+        while(min<max){
+            mid = (min+max)/2;
+            if(findNum< arr[mid]){
+                max = mid;
+            }
+            else if(findNum> arr[mid]){
+                min = mid;
+            }
+            else if(findNum==arr[mid]) return mid;
+        }
+        return -1;
     }
 }
