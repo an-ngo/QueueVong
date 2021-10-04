0b45c05a737ad1a5f9981a4abb25f19631f440d3
an.ngo <phowflowf135@gmail.com>
2021-10-04 11:16:20 +0700

    m

---
+++ binarySearch/src/BinarySearch.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.Collections;
+
+public class BinarySearch {
+
+    public static void main(String[] args) {
+    int [] arr = {9,8,6,7,4,5,3,1,2,0};
+        Arrays.sort(arr);
+        for (int i :
+                arr) {
+            System.out.println(i);
+        }
+    }
+}
